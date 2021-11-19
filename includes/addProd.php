450b4e201f0bc2d6825cc8c66b90ab44256c8c47
oskar-dah <oskdah-9@student.ltu.se>
2021-11-19 09:14:47 +0100

    you can now add prods but category is ignored

--- includes/addProd.php
+++ includes/addProd.php
@@ -1,17 +1,54 @@
 <?php
   include_once 'dbHandler.php';
-  
+
   $name = $_POST['products'];
   $price = $_POST['price'];
   $Amount = $_POST['amount'];
   $category = $_POST['category'];
 
-  $sql = "INSERT INTO products VALUES (null, '$name', '$price', '$Amount', '$category');";
+  /*function checkIfExist(){
+    $query = "SELECT * FROM products WHERE p_name = '$name' AND stock = '$Amount' AND price = '$price';";
+    $select = "SELECT idProduct FROM products WHERE p_name = '$name' AND stock = '$Amount' AND price = '$price';";
+    $queryResult = mysqli_query($conn, $query);
 
+    $row = mysqli_fetch_assoc($queryResult);
+    $idValue = $row['idProduct'];
+
+    if(mysqli_num_rows($result)>0){
+      $sql = "UPDATE products SET stock = stock + $price WHERE idProduct = '$idValue';"
+    }else{
+      $sql = "INSERT INTO products VALUES (null, '$name', '$price', '$Amount');";
+    }
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    mysqli_query($conn, $sql);
+  }*/
+  
+  $query = "SELECT * FROM products WHERE p_name = '$name' AND price = '$price';";
+  $queryResult = mysqli_query($conn, $query);
+
+  $row = mysqli_fetch_assoc($queryResult);
+  $idValue = $row['idProduct'];
+
+  if(mysqli_num_rows($queryResult)>0){
+    $sql = "UPDATE products SET stock = stock + $Amount WHERE idProduct = '$idValue';";
+  }else{
+    $sql = "INSERT INTO products VALUES (null, '$name', '$price', '$Amount');";
+  }
   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
   mysqli_query($conn, $sql);
+  
+  header("Location: ../index.php?submit=success");
 
+/*
+  $row = mysqli_fetch_assoc($result);
+  $id = $row['idProduct'];
+  $insert = "INSERT INTO category VALUES (null, '$id', '$category');";
 
+  mysqli_query($conn, $insert);
 
-  header("Location: ../test.php?submit=success");
+ */
+  
+  
+  //Källor
+  //https://stackoverflow.com/questions/18733545/selected-value-get-from-db-into-dropdown-select-box-option-using-php-mysql-error
 ?>
--- includes/removeProd.php
+++ includes/removeProd.php
@@ -4,8 +4,10 @@
   $ID = $_POST['productID'];
   
   //$sql = "INSERT INTO product VALUES (0,'$name', '$price', '$Amount', '$category');";
+  $delete = "DELETE FROM category WHERE product_idProduct = '$ID';";
+  mysqli_query($conn, $delete);
   $sql = "DELETE FROM products WHERE idProduct = '$ID';";
   mysqli_query($conn, $sql);
 
-  header("Location: ../test.php?submit=success");
+  header("Location: ../index.php?submit=success");
 ?>
