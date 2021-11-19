80bf11b4637b3f469248116444aa40cd7a96b319
oskar-dah <oskdah-9@student.ltu.se>
2021-11-19 13:29:59 +0100

    add works, not category

--- includes/addProd.php
+++ includes/addProd.php
@@ -5,23 +5,6 @@
   $price = $_POST['price'];
   $Amount = $_POST['amount'];
   $category = $_POST['category'];
-
-  /*function checkIfExist(){
-    $query = "SELECT * FROM products WHERE p_name = '$name' AND stock = '$Amount' AND price = '$price';";
-    $select = "SELECT idProduct FROM products WHERE p_name = '$name' AND stock = '$Amount' AND price = '$price';";
-    $queryResult = mysqli_query($conn, $query);
-
-    $row = mysqli_fetch_assoc($queryResult);
-    $idValue = $row['idProduct'];
-
-    if(mysqli_num_rows($result)>0){
-      $sql = "UPDATE products SET stock = stock + $price WHERE idProduct = '$idValue';"
-    }else{
-      $sql = "INSERT INTO products VALUES (null, '$name', '$price', '$Amount');";
-    }
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    mysqli_query($conn, $sql);
-  }*/
   
   $query = "SELECT * FROM products WHERE p_name = '$name' AND price = '$price';";
   $queryResult = mysqli_query($conn, $query);
@@ -38,21 +21,6 @@
   mysqli_query($conn, $sql);
   
   header("Location: ../index.php?submit=success");
-
-/*
-  $row = mysqli_fetch_assoc($result);
-  $id = $row['idProduct'];
-  $insert = "INSERT INTO category VALUES (null, '$id', '$category');";
-
-  mysqli_query($conn, $insert);
-
-<<<<<<< HEAD
- */
-  
-  
   //Källor
   //https://stackoverflow.com/questions/18733545/selected-value-get-from-db-into-dropdown-select-box-option-using-php-mysql-error
-=======
-  header("Location: ../index.php?submit=success");
->>>>>>> 40495a13339e12ae7728f5afde606efba877aaf6
 ?>
