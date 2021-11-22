7121fcb3ffbee0a8cc29ff8da103184c66253935
Ludvig Hedlund <ludvig.hedlund@hotmail.se>
2021-11-22 17:22:23 +0100

    cart now print session shopper and now able to remove item from cart

--- shopCart/shoppingCart.php
+++ shopCart/shoppingCart.php
@@ -1,57 +1,61 @@
 <?php
 	include_once '../includes/dbHandler.php';
+	session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
 <body>
 
-<h1>Your shopping cart (WIP)</h1>
+<h1>Your shopping cart</h1>
 
 <b>
 <p>
 <?php
-    $shopper = 1; //to be changed when loginSystem is done
-	$sql = "SELECT * FROM shopping_cart WHERE customer_idCustomer=$shopper;";
-	
-	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	
-	$result = mysqli_query($conn, $sql);
-	$checkResult = mysqli_num_rows($result);
-	
-	if($checkResult > 0){
-		while($productNr = mysqli_fetch_assoc($result)){
-			$sql2 = "SELECT p_name, price FROM products WHERE idProduct=$productNr[product_idProduct];";
 
-			$result2 = mysqli_query($conn, $sql2);
-			$checkResult2 = mysqli_num_rows($result2);
+	try {
+		$shopper = $_SESSION['idCustomer'];
+		$sql = "SELECT * FROM shopping_cart WHERE customer_idCustomer=$shopper;";
+	
+		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	
+		$result = mysqli_query($conn, $sql);
+		$checkResult = mysqli_num_rows($result);
+	
+		if($checkResult > 0){
+			while($productNr = mysqli_fetch_assoc($result)){
+				$sql2 = "SELECT p_name, price FROM products WHERE idProduct=$productNr[product_idProduct];";
 
-			$rowProduct = mysqli_fetch_assoc($result2);
-			echo "<br>" . "Name: " . $rowProduct['p_name'] . "<br>";
-			echo "Price: " . $rowProduct['price'] . "<br>";
-			//removeProdFromCartButton($shopper, idProduct);
-			
-		}
-	}  
+				$result2 = mysqli_query($conn, $sql2);
+				$checkResult2 = mysqli_num_rows($result2);
 
-	//https://stackoverflow.com/questions/30051961/how-can-i-make-an-html-button-that-passes-a-parameter
+				$rowProduct = mysqli_fetch_assoc($result2);
+				echo "<br>" . "Name: " . $rowProduct['p_name'] . "<br>";
+				echo "Price: " . $rowProduct['price'] . "<br>";
+				removeProdFromCartButton($shopper, $productNr['product_idProduct']);
+
+			}
+		}  
+	} catch (Exception $e) {
+		?>
+		<meta http-equiv="refresh" content="0; url=../signUp.php" />
+		<?php
+	}
+
 	function removeProdFromCartButton($shopper, $productID){ ?>
-		<?php echo $shopper; ?>
+
 		<form action="removeProdFromCart.php" method = "POST">
 
-     	<input type="hidden"
-            name="shopper"
-            value=<?php echo $shopper; ?>> 
-
 		<input type="hidden"
-            name="productID"
-            value=<?php echo $productID; ?>>
+			name="productID"
+			value=<?php echo $productID; ?>>
 
-     	<input type="submit" 
-            value="Ta Bort">
+		 <input type="submit" 
+			value="Ta Bort">
 		</form>
 	<?php
-	}
+	}  
+	
 ?>
 </p>
 </b>
