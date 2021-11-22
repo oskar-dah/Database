5cbfce3804151973abc6d951005b0e02da743285
tonytonfisk2 <tony6781@hotmail.com>
2021-11-22 10:03:51 +0100

    hhhh

--- includes/addProd.php
+++ includes/addProd.php
@@ -5,29 +5,8 @@
   $price = $_POST['price'];
   $Amount = $_POST['amount'];
   $category = $_POST['category'];
-<<<<<<< HEAD
   $sql = "INSERT INTO products VALUES (0, '$name', $price, $Amount, '$category');";
   mysqli_query($conn, $sql);
 
   header("Location: ../index.php?submit=success");
-=======
-  
-  $query = "SELECT * FROM products WHERE p_name = '$name' AND price = '$price';";
-  $queryResult = mysqli_query($conn, $query);
-
-  $row = mysqli_fetch_assoc($queryResult);
-  $idValue = $row['idProduct'];
-
-  if(mysqli_num_rows($queryResult)>0){
-    $sql = "UPDATE products SET stock = stock + $Amount WHERE idProduct = '$idValue';";
-  }else{
-    $sql = "INSERT INTO products VALUES (null, '$name', '$price', '$Amount');";
-  }
-  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-  mysqli_query($conn, $sql);
-  
-  header("Location: ../index.php?submit=success");
-  //Källor
-  //https://stackoverflow.com/questions/18733545/selected-value-get-from-db-into-dropdown-select-box-option-using-php-mysql-error
->>>>>>> db0bd20bdbfad9b5fe4a36edef693a6304f79dca
 ?>
