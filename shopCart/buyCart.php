3800ea17303646a82cca35d12ecb8811ccf490a0
Ludvig Hedlund <ludvig.hedlund@hotmail.se>
2021-11-24 17:48:34 +0100

    buy product and reduce the stock, but the history is not done

--- shopCart/buyCart.php
+++ shopCart/buyCart.php
@@ -2,6 +2,39 @@
 
 session_start();
 include_once '../includes/dbHandler.php';
+?>
+<html>
+<head> <link rel="stylesheet" href="../design/index.css?v=<?php echo time(); ?>"> 
+</head>
+</html>
+
+<?php
+if(isset($_SESSION["idCustomer"])){
+	if($_SESSION["user_type"] == "A"){
+		echo "<ul class=\"navbar\">";
+		echo "<li> <a href = '../index.php'> Web-shoppen </a></li>";
+		echo "<li><a href = 'shoppingCart.php'> Shopping cart </a> </li>";
+		echo "<li><a href = '../manageProducts.php'> Manage products </a> </li>";
+		echo "<li><a href = '../includes/logout.inc.php'> Log out </a></li>";
+		echo "</ul>";
+	}
+	else if($_SESSION["user_type"] == "U"){
+		echo '<ul class="navbar">';
+		echo "<li> <a href = '../index.php'> Web-shoppen </a></li>";
+		echo "<li><a href = 'shoppingCart.php'> Shopping cart </a> </li>";
+		echo "<li><a href = '../includes/logout.inc.php'> Log out </a></li>";
+		echo "</ul>";	
+	}
+}
+else{
+	echo '<ul class="navbar">';
+	echo "<li> <a href = '../index.php'> Web-shoppen </a></li>";
+	echo "<li><a href = '../signIn.php'> Sign in </a> </li>";
+	echo "<li><a href = '../signUp.php'> Sign up </a> </li>";
+	echo "<li><a href = 'shoppingCart.php'> Shopping cart </a> </li>";
+	echo "</ul>";
+}
+
 $shopper = $_SESSION['idCustomer'];
 $sql = "SELECT * FROM shopping_cart WHERE customer_idCustomer=$shopper;";
 
@@ -25,13 +58,39 @@ if($checkResult > 0){
             array_push($deniedProducts, $rowStock['p_name']);
             $allowPurchase = false;
         }
-        //$sql = "UPDATE products SET stock = stock - $productNr['amount'] WHERE idProduct = $productNr[product_idProduct];";
-
     }
+
     if($allowPurchase){
         echo "HATA LÖVEN";
+        echo $sql;
+
+        $result = mysqli_query($conn, $sql);
+
+        while($amount = mysqli_fetch_assoc($result)){
+            $a = $amount['amount'];
+            $sqlUpdate = "UPDATE products SET stock = stock - $a WHERE idProduct = $amount[product_idProduct];";
+            mysqli_query($conn, $sqlUpdate);
+
+            //KOM IHÅG ATT RENSA SHOPPING CART
+        }
+        header("Location: ../index.php?submit=success");
+
     } else {
-        echo "DIGIMON";//neka köp, printa varor som nekades och ta bort dessa från korgen
+        echo "error: dessa produkter finns inte i önskad mängd: <br>";
+
+        for ($i = 0; $i < count($deniedProducts); $i++) {
+            echo $deniedProducts[$i] . "<br>";
+        }
+
+        ?>
+        <html>
+        <form action="shoppingCart.php" method = "POST">
+		    <input type="submit" 
+			value="Ok">
+		</form>
+        </html>
+
+        <?php
     }
 }  
 ?>
