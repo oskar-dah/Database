b6dd7b298e3f40d105e424b18070e6219c1f2ea0
tonytonfisk2 <tony6781@hotmail.com>
2021-12-15 10:36:34 +0100

    uppdaterad databas

--- webshopdb.sql
+++ webshopdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2021 at 01:09 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2021 at 10:35 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,16 +27,25 @@ SET time_zone = "+00:00";
 -- Table structure for table `boughtproducts`
 --
 
-DROP TABLE IF EXISTS `boughtproducts`;
-CREATE TABLE IF NOT EXISTS `boughtproducts` (
-  `idBoughtProducts` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `boughtproducts` (
+  `idBoughtProducts` int(11) NOT NULL,
   `products_idProduct` int(11) NOT NULL,
   `shipment_idShipment` int(11) NOT NULL,
   `priceAtPurchase` float NOT NULL,
-  PRIMARY KEY (`idBoughtProducts`),
-  KEY `products_idProduct` (`products_idProduct`),
-  KEY `shipment_idShipment` (`shipment_idShipment`)
+  `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `boughtproducts`
+--
+
+INSERT INTO `boughtproducts` (`idBoughtProducts`, `products_idProduct`, `shipment_idShipment`, `priceAtPurchase`, `amount`) VALUES
+(1, 13, 2, 222, 22),
+(2, 14, 3, 250, 1),
+(3, 14, 4, 250, 1),
+(4, 15, 4, 5555, 1),
+(5, 16, 5, 245242, 1),
+(6, 14, 6, 250, 2);
 
 -- --------------------------------------------------------
 
@@ -44,26 +53,25 @@ CREATE TABLE IF NOT EXISTS `boughtproducts` (
 -- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE IF NOT EXISTS `customer` (
-  `idCustomer` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `customer` (
+  `idCustomer` int(11) NOT NULL,
   `username` varchar(45) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `forname` varchar(45) DEFAULT NULL,
-  `lastname` varchar(45) DEFAULT NULL,
-  `phoneNr` varchar(45) DEFAULT NULL,
-  `address` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idCustomer`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `password` varchar(45) NOT NULL,
+  `forname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `phoneNr` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `user_type` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`idCustomer`, `username`, `password`, `forname`, `lastname`, `phoneNr`, `address`, `email`) VALUES
-(1, 'pi', 'pi', NULL, NULL, NULL, NULL, NULL),
-(2, 'bob', 'bob', 'bob', 'Andersson', '070-0872456', 'gatan 34', 'bob@bob.se');
+INSERT INTO `customer` (`idCustomer`, `username`, `password`, `forname`, `lastname`, `phoneNr`, `address`, `email`, `user_type`) VALUES
+(2, 'tonytonfisk2', 'hejsan123', 'Tony', 'Tonytonfisk', '0733591437', 'Laboratorievägen 25', 'tonzha-9@student.ltu.se', 'U'),
+(4, 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'A');
 
 -- --------------------------------------------------------
 
@@ -71,26 +79,23 @@ INSERT INTO `customer` (`idCustomer`, `username`, `password`, `forname`, `lastna
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `idProduct` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `products` (
+  `idProduct` int(11) NOT NULL,
   `p_name` varchar(45) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `stock` int(11) DEFAULT NULL,
-  `category` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idProduct`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+  `category` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`idProduct`, `p_name`, `price`, `stock`, `category`) VALUES
-(8, 'tv', 20, 250, 'Screen'),
-(9, 'tv', 20, 20, 'Screen'),
-(10, 'Pineapple', 20, 5, 'Fruit'),
-(11, 'Banana', 25, 5, 'Fruit'),
-(13, 'Samsung 24\"', 2495, 16, 'Screen');
+(13, 'tv', 222, 0, 'screen'),
+(14, 'Chips', 250, 246, 'food'),
+(15, 'banan', 5555, 554, 'ffff'),
+(16, '3334afa', 245242, 26261, 'asfsfa');
 
 -- --------------------------------------------------------
 
@@ -98,17 +103,21 @@ INSERT INTO `products` (`idProduct`, `p_name`, `price`, `stock`, `category`) VAL
 -- Table structure for table `reviews`
 --
 
-DROP TABLE IF EXISTS `reviews`;
-CREATE TABLE IF NOT EXISTS `reviews` (
+CREATE TABLE `reviews` (
   `customer_idCustomer` int(11) NOT NULL,
   `product_idProduct` int(11) NOT NULL,
   `comment` tinytext NOT NULL,
   `rating` int(11) NOT NULL,
-  `reviewID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`reviewID`),
-  KEY `customer_idCustomer` (`customer_idCustomer`),
-  KEY `product_idProduct` (`product_idProduct`)
+  `reviewID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`customer_idCustomer`, `product_idProduct`, `comment`, `rating`, `reviewID`) VALUES
+(2, 14, 'fafaf', 5, 145),
+(2, 15, 'fgafafafafa', 5, 146);
 
 -- --------------------------------------------------------
 
@@ -116,14 +125,22 @@ CREATE TABLE IF NOT EXISTS `reviews` (
 -- Table structure for table `shipment`
 --
 
-DROP TABLE IF EXISTS `shipment`;
-CREATE TABLE IF NOT EXISTS `shipment` (
-  `idShipment` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `shipment` (
+  `idShipment` int(11) NOT NULL,
   `customer_idCustomer` int(11) NOT NULL,
-  `shippingAddr` varchar(45) NOT NULL,
-  PRIMARY KEY (`idShipment`),
-  KEY `customer_idCustomer` (`customer_idCustomer`)
+  `shippingAddr` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `shipment`
+--
+
+INSERT INTO `shipment` (`idShipment`, `customer_idCustomer`, `shippingAddr`) VALUES
+(2, 2, 'sss'),
+(3, 2, 'rr'),
+(4, 2, 'asda'),
+(5, 2, '222'),
+(6, 4, 'k');
 
 -- --------------------------------------------------------
 
@@ -131,16 +148,99 @@ CREATE TABLE IF NOT EXISTS `shipment` (
 -- Table structure for table `shopping_cart`
 --
 
-DROP TABLE IF EXISTS `shopping_cart`;
-CREATE TABLE IF NOT EXISTS `shopping_cart` (
-  `idCart` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `shopping_cart` (
+  `idCart` int(11) NOT NULL,
   `customer_idCustomer` int(11) NOT NULL,
   `product_idProduct` int(11) NOT NULL,
-  `shippingAddr` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCart`),
-  KEY `product_idProduct` (`product_idProduct`),
-  KEY `customer_idCustomer` (`customer_idCustomer`)
+  `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `boughtproducts`
+--
+ALTER TABLE `boughtproducts`
+  ADD PRIMARY KEY (`idBoughtProducts`),
+  ADD KEY `products_idProduct` (`products_idProduct`),
+  ADD KEY `shipment_idShipment` (`shipment_idShipment`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`idCustomer`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`idProduct`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`reviewID`),
+  ADD KEY `customer_idCustomer` (`customer_idCustomer`),
+  ADD KEY `product_idProduct` (`product_idProduct`);
+
+--
+-- Indexes for table `shipment`
+--
+ALTER TABLE `shipment`
+  ADD PRIMARY KEY (`idShipment`),
+  ADD KEY `customer_idCustomer` (`customer_idCustomer`);
+
+--
+-- Indexes for table `shopping_cart`
+--
+ALTER TABLE `shopping_cart`
+  ADD PRIMARY KEY (`idCart`),
+  ADD KEY `product_idProduct` (`product_idProduct`),
+  ADD KEY `customer_idCustomer` (`customer_idCustomer`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `boughtproducts`
+--
+ALTER TABLE `boughtproducts`
+  MODIFY `idBoughtProducts` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `idCustomer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `reviewID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
+
+--
+-- AUTO_INCREMENT for table `shipment`
+--
+ALTER TABLE `shipment`
+  MODIFY `idShipment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `shopping_cart`
+--
+ALTER TABLE `shopping_cart`
+  MODIFY `idCart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- Constraints for dumped tables
